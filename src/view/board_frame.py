c0aaf8dd70eb589d88ca1fef4af324e2a65cdb83
sama-gharib <sama.gharib-ali-barura@proton.me>
2025-03-10 19:26:07 +0100

    Add documentation

--- src/view/board_frame.py
+++ src/view/board_frame.py
@@ -4,10 +4,16 @@ from view.frame import Frame
 from view.slot_button import SlotButton
 
 class BoardFrame (Frame):
+    '''
+        This widget makes displaying a board easy !
+    '''
+
     def __init__(self, board, fill = "dark grey", position = pg.Vector2(0, 0), size = pg.Vector2(1, 1), children = []):
         super().__init__(True, fill, position, size, children)
         self._board = board
         
         for line in range(self._board.board_size):
             for column in range(self._board.board_size):
+                # SlotButton position is expected to be in board
+                # position, so this works :
                 self._children.append(SlotButton(board, pg.Vector2(column, line)))
--- src/view/slot_button.py
+++ src/view/slot_button.py
@@ -4,9 +4,21 @@ from view.button import Button
 from model.board import Board
 
 class SlotButton (Button):
+    '''
+        This widget holds a reference to a board in order
+        to auto update when player puts a new token.
+    '''
 
     def __init__(self, board, position = pg.Vector2(0, 0), children = []):
-        slot_size = 1/board.board_size
+        '''
+            Warning ! Here, position is expected to be in board
+            coordinates format, not in proportion as expected
+            usually with widgets !!
+            I.e : For an 8 slots wide board, position is expected
+            to be between (0, 0) and (7, 7)
+        '''
+
+        slot_size = 1/board.board_size # In proportion
 
         super().__init__(
             lambda: print("TODO!!"),
@@ -16,13 +28,14 @@ class SlotButton (Button):
             children
         )
         self._board = board
-        self._slot = position
+        self._slot = position # (column, line)
 
     # Overrides
 
     def draw(self, surface):
         super().draw(surface)
         
+        # Available since Python 3.10
         match self._board.get_at(int(self._slot.y), int(self._slot.x)):
             case Board.WHITE:
                 pg.draw.ellipse(surface, 'white', self._absolute.as_rect())
@@ -31,6 +44,7 @@ class SlotButton (Button):
             case Board.EMPTY:
                 pass
 
+        # Draw little cursor on hover
         if self._state == "hover":
             col = [i * 0.9 for i in self.get_true_color()]
             rec = self._absolute.as_rect()
